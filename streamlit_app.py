ab649ef1401e09a94c6c7a7ffd05007f93338eec
makarandkurundkar <makarand.kurundkar@gmail.com>
2026-02-09 09:06:05 +0530

    Update smoothie order display logic
    Commented out the display of the API response and added a dataframe display for the JSON response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,8 @@ if ingredients_list:
 
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)
+    #st.text(smoothiefroot_response)
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
   
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
