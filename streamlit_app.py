a484c25fc3bd0852561eddb0d5f621e2d261ffd1
makarandkurundkar <makarand.kurundkar@gmail.com>
2026-02-09 09:02:25 +0530

    Fix variable name for smoothie fruit response

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
     import requests
-    smoothiefroot= requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     st.text(smoothiefroot_response)
   
     if time_to_insert:
