c5e64f8221b5fe21d3ba43e56a2886f590bb714a
makarandkurundkar <makarand.kurundkar@gmail.com>
2026-02-09 09:27:23 +0530

    Enhance smoothie options with additional column and display
    Updated the DataFrame to include 'SEARCH_ON' column and converted it to a pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests 
+import pandas as pd
+
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -17,8 +19,12 @@ session = cnx.session()
 
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
